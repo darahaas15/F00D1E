0ccf9b7aa04f69a8d045b856832552579cd59946
Darahaas Yajamanyam <darahaas15@gmail.com>
2021-08-08 03:27:59 +0530

    Completed empty cart page

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const PORT = process.env.PORT || 3000;
 // assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
 // set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+// routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+  res.render('customers/cart');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);

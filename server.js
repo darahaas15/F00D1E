2a84043e29d91fd436c1e92a1808cb0eeb29cb90
Darahaas Yajamanyam <darahaas15@gmail.com>
2021-08-09 03:13:03 +0530

    login form bsic template made

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/', (req, res) => {
 app.get('/cart', (req, res) => {
   res.render('customers/cart');
 });
+app.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+app.get('/register', (req, res) => {
+  res.render('auth/register');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
